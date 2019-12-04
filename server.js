315638d3e69bbca0479660b75b9ef89a653f9a78
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-04 07:08:37 +0000

    use dotenv for local env for server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3001
 
 const app = express()
 
+require('dotenv').config()
+
 app.use(require("./app/functions/lib/src/requestCompany.js"))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
