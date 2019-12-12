eca31afdbd3ecfe3ad588e97df200dbfd723f4ec
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-12 13:59:52 +0000

    needed

--- app/src/utils/request.ts
+++ app/src/utils/request.ts
@@ -2,11 +2,16 @@
 
 const domain = window.location.href.indexOf("localhost") > -1 ? "http://localhost:3001": "https://us-central1-blink-3b651.cloudfunctions.net"
 
-
 const requestCompany = async (companyId: string) => {
     const response = await fetch(`${domain}/company/${companyId}`, { mode: 'cors' });
     const body = await response.json();
     return body;
 }
 
-export { requestCompany }
+const requestSignificantPersons = async (companyId: string) => {
+    const response = await fetch(`${domain}/persons-with-significant-control/${companyId}`, { mode: 'cors' });
+    const body = await response.json();
+    return body;
+}
+
+export { requestCompany, requestSignificantPersons }
