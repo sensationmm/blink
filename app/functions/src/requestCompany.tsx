6b019aa9206b8558fb077b85f989613d3e271b24
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-13 15:16:11 +0000

    fix

--- app/functions/src/requestCompany.tsx
+++ app/functions/src/requestCompany.tsx
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const companyFunctions = require('firebase-functions');
 const companyCors = require('cors');
 const companyExpress = require('express');
 const request = require('request');
@@ -16,7 +16,7 @@ companyServer.get('*/:companyId', function (req: any, res: any) {
     const headerOption = {
         "url": `https://api.companieshouse.gov.uk/company/${companyId}`,
         "headers": {
-            "Authorization": `${process.env.COMPANIES_HOUSE_API_KEY || functions.config().companies_house_api.key}`
+            "Authorization": `${process.env.COMPANIES_HOUSE_API_KEY || companyFunctions.config().companies_house_api.key}`
         }
     };
 
