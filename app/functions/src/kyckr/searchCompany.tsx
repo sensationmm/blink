7069e4f00ceae3991e1d5264d44840cfccb04364
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-19 11:32:55 +0000

    kyckr endpoint

--- app/functions/src/kyckr/searchCompany.tsx
+++ app/functions/src/kyckr/searchCompany.tsx
@@ -3,28 +3,27 @@ const kyckrCors = require('cors');
 const kyckrExpress = require('express');
 var soap = require('soap');
 
-
 const kyckrServer = kyckrExpress();
 
 kyckrServer.use(kyckrCors());
 
-kyckrServer.get('/', function (req: any, res: any) {
+kyckrServer.get('*/:query/:countryISOCode', function (req: any, res: any) {
+
+    const { query, countryISOCode } = req.params;
+
+    console.log("query", query);
 
     const url = 'https://testws.kyckr.eu/gbronboarding.asmx?wsdl';
-    var args = { "companyName": "eleven fs", "termsAndConditions": "true" };
+    var args = { email: "terry.cordeiro@11fs.com", password: "c72fde3", countryISOCode, companyName: query, termsAndConditions: true };
 
 
-    var auth = "Basic " + new Buffer("terry.cordeiro@11fs.com" + ":" + "6c72fde3").toString("base64");
-
-    // soap.createClient(url, { wsdl_headers: {Authorization: auth} }, function(err, client) {
-    // });
+    const auth = "Basic " + JSON.stringify({"terry.cordeiro@11fs.com":"c72fde3"})
 
     soap.createClient(url, { wsdl_headers: { Authorization: auth } }, function (err: any, client: any) {
         // console.log("error", err)
         // console.log("client", client)
         client.CompanySearch(args, function (err: any, result: any) {
-            console.log("client", client);
-            res.send("here sd")
+            res.send(JSON.parse(result));
         });
     });
 })
