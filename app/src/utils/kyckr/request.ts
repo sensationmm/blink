99148ed0d9531c3457a7fa92a33ed23fc78e5ea5
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-20 11:07:36 +0000

    gitignore

--- app/src/utils/kyckr/request.ts
+++ app/src/utils/kyckr/request.ts
@@ -1,6 +1,6 @@
 
 
-const domain = window.location.href.indexOf("localhost") > -1 ? "http://localhost:3001" : "https://us-central1-blink-3b651.cloudfunctions.net"
+const domain = window.location.href.indexOf("localhost") > -1 ? "http://localhost:3001" : "https://us-central1-blink-3b651.cloudfunctions.net";
 
 const searchCompany = async (query: string, countryISOCode: string = "GB") => {
     const response = await fetch(`${domain}/kyckrSearchCompany/${query}/${countryISOCode}`, { mode: 'cors' });
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 
 require('dotenv').config()
 
-app.use(require("./app/functions/lib/src/requestCompany.js"))
+// app.use(require("./app/functions/lib/src/kyckr/searchCompany.js"))
+
+app.use(require("./app/functions/lib/src/kyckr/requestCompanyProfile.js"))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
