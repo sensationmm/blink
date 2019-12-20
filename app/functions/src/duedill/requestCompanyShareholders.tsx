94361d49a04d4fa2650d7805e93d79af90877035
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-20 18:07:26 +0000

    all along - the url was incorrect

--- app/functions/src/duedill/requestCompanyShareholders.tsx
+++ app/functions/src/duedill/requestCompanyShareholders.tsx
@@ -11,39 +11,23 @@ duedillCompanyShareholdersServer.get('*/:countryCode/:companyId', function (req:
 
     const { companyId, countryCode } = req.params;
 
-    // console.log("companyId", companyId);
+    const options = {
+        "headers": {
+            "Accept": "application/json",
+            'Authorization': `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`,
+            // "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`
+        },
+        "url": `https://duedil.io/v4/company/${countryCode}/${companyId}/shareholders.json`,
+        "credentials": 'include'
+    };
 
-    // console.log("countryCode", countryCode)
-
-    // console.log("DUE_DILL_API_KEY", process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key)
-
-    // const headerOption = {
-    //     "url": `https://api.companieshouse.gov.uk/company/${countryCode}/${companyId}/shareholders.json`,
-    //     "credentials": 'include',
-    //     "headers": {
-    //         'Authorization': `X-AUTH-TOKEN ${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`,
-    //         "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`
-    //     }
-    // };
-
-    console.log("res", res);
-    console.log("headers", res.headers);
-
-    duedillCompanyShareholdersRequest.get({
-        headers: { "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}` },
-        url: `https://api.companieshouse.gov.uk/company/${countryCode}/${companyId}/shareholders.json`,
-    }, function (error: any, response: any, body: any) {
+    duedillCompanyShareholdersRequest(options, function (error, response, body) {
         if (error) {
             console.log("error", error);
         }
         console.log("body", body);
-        res.send(body)
-    });
-
-
-    // duedillCompanyShareholdersRequest(headerOption, function (error: any, response: any, body: any) {
-    //     res.send(JSON.parse(body))
-    // });
+        res.send(body);
+    })
 })
 
 module.exports = duedillCompanyShareholdersFunctions.https.onRequest(duedillCompanyShareholdersServer)
