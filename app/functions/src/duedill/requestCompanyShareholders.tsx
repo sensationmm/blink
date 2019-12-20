e40b2cc98eceb05cd591e6aaca208dc1533635e0
Nick Procopiou <nick.procopiou@11fs.com>
2019-12-20 14:30:46 +0000

    try the get

--- app/functions/src/duedill/requestCompanyShareholders.tsx
+++ app/functions/src/duedill/requestCompanyShareholders.tsx
@@ -17,18 +17,31 @@ duedillCompanyShareholdersServer.get('*/:countryCode/:companyId', function (req:
 
     console.log("DUE_DILL_API_KEY", process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key)
 
-    const headerOption = {
-        "url": `https://api.companieshouse.gov.uk/company/${countryCode}/${companyId}/shareholders.json`,
-        "credentials": 'include',
-        "headers": {
-            'Authorization': `X-AUTH-TOKEN ${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`,
-            "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`
-        }
-    };
+    // const headerOption = {
+    //     "url": `https://api.companieshouse.gov.uk/company/${countryCode}/${companyId}/shareholders.json`,
+    //     "credentials": 'include',
+    //     "headers": {
+    //         'Authorization': `X-AUTH-TOKEN ${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`,
+    //         "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}`
+    //     }
+    // };
 
-    duedillCompanyShareholdersRequest(headerOption, function (error: any, response: any, body: any) {
-        res.send(JSON.parse(body))
+
+    duedillCompanyShareholdersRequest.get({
+        headers: { "X-AUTH-TOKEN": `${process.env.DUE_DILL_API_KEY || duedillCompanyShareholdersFunctions.config().due_dill_api.key}` },
+        url: `https://api.companieshouse.gov.uk/company/${countryCode}/${companyId}/shareholders.json`,
+    }, function (error: any, response: any, body: any) {
+        if (error) {
+            console.log("error", error);
+        }
+        console.log("response", response);
+        res.send(body)
     });
+
+
+    // duedillCompanyShareholdersRequest(headerOption, function (error: any, response: any, body: any) {
+    //     res.send(JSON.parse(body))
+    // });
 })
 
 module.exports = duedillCompanyShareholdersFunctions.https.onRequest(duedillCompanyShareholdersServer)
