6e444b8cad8a6b1e08ef67975b4e86afd109826f
Nick Procopiou <nick.procopiou@11fs.com>
2020-01-07 10:33:42 +0000

    confusing docs

--- app/functions/src/trulioo/searchCompany.tsx
+++ app/functions/src/trulioo/searchCompany.tsx
@@ -10,15 +10,17 @@ truliooSearchCompanyServer.use(truliooSearchCompanyCors());
 truliooSearchCompanyServer.get('*/:query', function (req: any, res: any) {
 
     // const { query } = req.params;
-
-
+    
     const headerOption = {
         // "url": `https://api.globaldatacompany.com/connection/v1/sayhello/${query}`,
-        "url": "https://api.globaldatacompany.com/connection/v1/testauthentication",
+        "url": "https://gateway.trulioo.com/trial/connection/v1/testauthentication",
         "headers": {
-            "x-trulioo-api-key": `${process.env.TRULIOO_API_KEY || truliooFunctions.config().trulioo_api.key}`
+            "x-trulioo-api-key": `${process.env.TRULIOO_API_KEY || truliooFunctions.config().trulioo_api.key}`,
+            // "Authorization": `${process.env.TRULIOO_API_KEY || truliooFunctions.config().trulioo_api.key}`
         }
     };
+
+    console.log("api key", truliooFunctions.config().trulioo_api.key)
 
     truliooSearchCompanyRequest(headerOption, function (error: any, response: any, body: any) {
         if (error) {
