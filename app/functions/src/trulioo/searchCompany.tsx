0637b3d095295be65e823a63ec238b94db121eae
Nick Procopiou <nick.procopiou@11fs.com>
2020-01-07 11:33:00 +0000

    get some test entities

--- app/functions/src/trulioo/searchCompany.tsx
+++ app/functions/src/trulioo/searchCompany.tsx
@@ -7,13 +7,14 @@ const truliooSearchCompanyServer = truliooSearchCompanyExpress();
 
 truliooSearchCompanyServer.use(truliooSearchCompanyCors());
 
-truliooSearchCompanyServer.get('*/:query', function (req: any, res: any) {
+truliooSearchCompanyServer.get('*/:countryCode', function (req: any, res: any) {
 
-    // const { query } = req.params;
+    const { countryCode } = req.params;
     
     const headerOption = {
         // "url": `https://api.globaldatacompany.com/connection/v1/sayhello/${query}`,
-        "url": "https://gateway.trulioo.com/trial/connection/v1/testauthentication",
+        // "url": "https://gateway.trulioo.com/trial/connection/v1/testauthentication",
+        "url": `https://gateway.trulioo.com/trial/configuration/v1/testentities/Identity Verification/${countryCode}`,
         "headers": {
             "x-trulioo-api-key": `${process.env.TRULIOO_API_KEY || truliooFunctions.config().trulioo_api.key}`
         }
