fee3e7e570bcb6134fbafe909e1a4b438faf2cf0
Nick Procopiou <nick.procopiou@11fs.com>
2020-01-08 12:59:29 +0000

    what a pain

--- app/functions/index.ts
+++ app/functions/index.ts
@@ -8,7 +8,7 @@ admin.initializeApp({
         "type": "service_account",
         "project_id": "blink-3b651",
         "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID || functions.config().service_account_private_key_id.key,
-        "private_key":  `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY || functions.config().service_account_private_key.key}\n-----END PRIVATE KEY-----\n`,
+        "private_key":  (`-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY || functions.config().service_account_private_key.key}\n-----END PRIVATE KEY-----\n`).replace(/\\n/g, '\n'),
         "client_email":  process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL || functions.config().service_account_client_email.key,
         "client_id":  process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID || functions.config().service_account_client_id.key,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
