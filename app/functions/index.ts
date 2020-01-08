829790e6b2731ce32a6a39a4ff753186b19d84e1
Nick Procopiou <nick.procopiou@11fs.com>
2020-01-08 11:18:51 +0000

    try again

--- app/functions/index.ts
+++ app/functions/index.ts
@@ -3,7 +3,7 @@
 const admin = require('firebase-admin');
 
 admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT.key || ""),
+    credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT.key ||  ""),
     databaseURL: "https://blink-3b651.firebaseio.com"
 });
 
