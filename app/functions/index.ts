fcdc2686832c5359fff34473fc68c7fb11ed7789
Nick Procopiou <nick.procopiou@11fs.com>
2020-01-08 12:46:39 +0000

    firebase config names cant include the word firebase - who knew

--- app/functions/index.ts
+++ app/functions/index.ts
@@ -3,7 +3,21 @@
 const admin = require('firebase-admin');
 const functions = require("firebase-functions");
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "blink-3b651",
+        "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID || functions.config().service_account_private_key_id.key,
+        "private_key":  `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY || functions.config().service_account_private_key.key}\n-----END PRIVATE KEY-----\n`,
+        "client_email":  process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL_ID || functions.config().service_account_client_email_id.key,
+        "client_id":  process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID || functions.config().service_account_client_email.key,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7ep8r%40blink-3b651.iam.gserviceaccount.com"
+    }),
+    databaseURL: "https://blink-3b651.firebaseio.com"
+});
 
 exports.duedillSearchCompany = require("./src/duedill/searchCompany");
 
