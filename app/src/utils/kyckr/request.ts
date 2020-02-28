917edf8f701f6ac2b0318e950df9f8ef1fa6f5aa
Nick Procopiou <nick.procopiou@11fs.com>
2020-02-28 12:45:26 +0000

    more routing fixes

--- app/src/utils/kyckr/request.ts
+++ app/src/utils/kyckr/request.ts
@@ -1,7 +1,7 @@
 
 const stringSimilarity = require('string-similarity');
 
-const domain = window.location.href.indexOf("localhost") > -1 ? "http://localhost:5001/blink-3b651/us-central1" : "https://us-central1-blink-3b651.cloudfunctions.net";
+const domain = window.location.href.indexOf("localhost") > -1 ? "http://localhost:5001/blink-3b651/us-central1" : "";
 
 const searchCompany = async (query: string, countryISOCode: string = "GB", orderReference: string) => {
     const response = await fetch(`${domain}/kyckrSearchCompany/${query}/${countryISOCode}/${orderReference}`, { mode: 'cors' });
