608cfc81b8f8523b41150c9345312bac6bb0cca2
Nick Procopiou <nick.procopiou@11fs.com>
2020-03-11 22:31:24 +0000

    force build

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,6 @@ import {
   withRouter
 } from "react-router-dom";
 
-
 // import CompaniesHouse from "./components/companies-house-2"
 import Kyckr from "./components/kyckr"
 import KyckrFilingSearch from "./components/kyckr/filing-search";
