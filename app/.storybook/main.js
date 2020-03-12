83987fe185417719c27fe757842b456ac69997c2
Kevin Reynolds <kevin.reynolds@11fs.com>
2020-03-12 15:31:05 +0000

    create-react-app eject

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/stories.js'],
   addons: [
-    '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs',
     '@storybook/addon-viewport/register',
+    '@storybook/preset-typescript',
   ],
 };
--- app/src/containers/Doc.tsx
+++ app/src/containers/Doc.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import * as Styled from "../components/styles";
 
 const Content = require('../doc/global.html');
-require('../doc/styles/jsdoc-default.css')
+require('../components/styles.jsdoc.css')
 
 const Doc = () => {
     const content = Content.replace(/src="scripts/gi, 'src="/doc').replace(/href="(global|index).html/gi, 'href="/doc').replace(/href="styles/gi, 'href="../doc/styles');
 
     return (
-        <Styled.MainSt style={{ width: 'calc(100vw - 60px)', height: '100vh' }} dangerouslySetInnerHTML={{ __html: content }} />
+        <Styled.MainSt className="jsdoc" style={{ width: 'calc(100vw - 60px)', height: '100vh' }} dangerouslySetInnerHTML={{ __html: content }} />
     )
 }
 
