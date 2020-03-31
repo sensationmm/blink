3eaf1baf19981b67569edb7c49a0720cd19cd8ea
Nick Procopiou <nick.procopiou@11fs.com>
2020-03-31 09:37:22 +0100

    revolut private key

--- app/functions/src/revolut/generateJWT.tsx
+++ app/functions/src/revolut/generateJWT.tsx
@@ -3,7 +3,7 @@ export {}
 const jwt = require('jsonwebtoken');
 const functions = require('firebase-functions');
 
-const REVOLUT_PRIVATE_KEY = (`-----BEGIN RSA PRIVATE KEY-----\n${process.env.REVOLUT_PRIVATE_KEY || functions.config().revolut_private.key}\n-----END RSA PRIVATE KEY-----\n`).replace(/\\n/g, '\n');
+const REVOLUT_PRIVATE_KEY = (`-----BEGIN RSA PRIVATE KEY-----\n${process.env.REVOLUT_PRIVATE_KEY || functions.config().revolut_private_key.key}\n-----END RSA PRIVATE KEY-----\n`).replace(/\\n/g, '\n');
 const REVOLUT_CLIENT_ID = (process.env.REVOLUT_CLIENT_ID || functions.config().revolut_client_id.key);
 const REVOLUT_ISSUER = (process.env.REVOLUT_ISSUER || functions.config().revolut_issuer.key);
 
