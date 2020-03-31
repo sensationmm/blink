46dbf8122e7cd8458f7e30d8fe1fc68c8ba4024a
Nick Procopiou <nick.procopiou@11fs.com>
2020-03-31 09:26:42 +0100

    missing import

--- app/functions/src/revolut/authenticateCallback.tsx
+++ app/functions/src/revolut/authenticateCallback.tsx
@@ -7,6 +7,7 @@ const express = require('express');
 const server = express();
 const admin = require("firebase-admin");
 const request = require('request');
+const generateJWT = require('./generateJWT');
 
 server.use(cors());
 
