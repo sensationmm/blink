d0030c7ff891a98417dd326f73093a6c205a8bd0
Nick Procopiou <nick.procopiou@11fs.com>
2020-03-31 11:42:35 +0100

    debug on staging

--- app/functions/src/revolut/generateJWT.tsx
+++ app/functions/src/revolut/generateJWT.tsx
@@ -7,6 +7,9 @@ const REVOLUT_PRIVATE_KEY = (`-----BEGIN RSA PRIVATE KEY-----\n${process.env.REV
 const REVOLUT_CLIENT_ID = (process.env.REVOLUT_CLIENT_ID || functions.config().revolut_client_id.key);
 const REVOLUT_ISSUER = (process.env.REVOLUT_ISSUER || functions.config().revolut_issuer.key);
 
+console.log("REVOLUT_PRIVATE_KEY for refresh", REVOLUT_PRIVATE_KEY)
+
+
 const aud = 'https://revolut.com' // Constant
 const payload = {
   "iss": REVOLUT_ISSUER,
