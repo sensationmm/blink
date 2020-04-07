ecada52b4064786f92992b999aea6a382bdb82ac
Nick Procopiou <nick.procopiou@11fs.com>
2020-04-07 08:15:09 +0100

    split out component code

---
+++ app/src/components/integrations/styles.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Icon = styled.div`
+margin-right: 30px;
+img {
+    width: 26px;
+}
+`;
+
+export const AccountDetails = styled.div`
+margin: 10px 30px 0 0;
+position: relative;
+`
+export const AccountName = styled.div`
+    margin: 20px 0 10px;
+    transform: translateY(10px);
+    display: block;
+`
+export const Item = styled.span`
+    margin-right: 20px;
+`
+
+export const AccountBalance = styled.div`
+    position: absolute; 
+    right: -20px;
+    font-size: 40px;
+    top: calc(50% - 40px);
+    transform: translateY(-50%);
+`
+
+export const TimeStamp = styled.div`
+    position: absolute; 
+    right: 20px;
+    font-size: 12px;
+    top: 20px;
+    color: #ccc;
+`
+export const Refresh = styled.img`
+    height: 20px;
+    opacity: 0.5;
+    margin: 0 10px -5px 10px;
+    &:hover {
+        opacity: 1;
+    }
+`
+
+export const LinkAccount = styled.button`
+    position: absolute;
+    bottom: 40px;
+    right: 40px;
+`
--- app/src/containers/Integrations.tsx
+++ app/src/containers/Integrations.tsx
@@ -20,11 +20,8 @@ import { Actions } from './modal-styles';
 import ReactJson from "react-json-view";
 import * as Styled from '../components/styles';
 import Button from "../components/button";
-import Box from '../layout/box';
-import Blocks from '../layout/blocks';
-import { Icon, AccountDetails, AccountName, AccountBalance, Item, TimeStamp, Refresh, LinkAccount } from "./integrations.styles";
-import { blinkMarkets, currencySymbols } from '../utils/config/blink-markets';
-import refreshIcon from '../svg/refresh_icon.svg'
+
+import Revolut from "../components/integrations/revolut";
 
 const Integrations = (props: any) => {
 
@@ -41,9 +38,6 @@ const Integrations = (props: any) => {
   const [changeAccountStatusToArchived, setChangeAccountStatusToArchived] = useState(true);
   const [accountFilter, setAccountFilter] = useState("ANY");
   const [searchString, setSearchString] = useState("");
-  const [bankAccounts, setBankAccounts] = useState();
-  const [counterparties, setCounterparties] = useState();
-  const [transactions, setBankAccountTransactions] = useState();
   const filterAccountTypes = ["Any", "Bank", "Revenue", "DirectCosts", "Expense", "Current", "Inventory", "Fixed", "Currliab", "termLiab", "Equity"]
 
   const toggleAccountStatusAndRefreshData = async () => {
@@ -51,186 +45,6 @@ const Integrations = (props: any) => {
     if (result.refresh) {
       onSelectTab(tabs.find((a: any) => a.name === "Accounts"))
     }
-  }
-
-  const getRevolutBankAccounts = async () => {
-    const accounts = await props.revolutGetBankAccounts();
-    setBankAccounts(accounts ?.map((account: any) => {
-      if (account.accounts) {
-        return {
-          ...account, accounts: account.accounts ?.map((pot: any) => {
-            return { ...pot, selectedCounterparty: "", pendingPaymentAmount: "" }
-          }) 
-        }
-      }
-    }))
-  }
-
-  const getRevolutCounterparties = async () => {
-    const counterparties = await props.revolutGetCounterparties();
-    setCounterparties(counterparties);
-  }
-
-  const getRevolutBankAccountTransactions = async () => {
-    const transactions = await props.revolutGetBankAccountTransactions();
-    // console.log(transactions)
-    setBankAccountTransactions("transactions");
-  }
-
-  const updateSelectedAccountCounterparty = (accountId: string, potIndex: number, counterPartyId: string) => {
-    setBankAccounts(bankAccounts ?.map((bankAccount: any) => {
-      if (bankAccount.id === accountId) {
-        bankAccount.accounts[potIndex].selectedCounterparty = counterPartyId
-      }
-      return bankAccount
-    }));
-  }
-
-  const updatePotPendingPaymentAmount = (accountId: string, potIndex: number, paymentAmount: string) => {
-    setBankAccounts(bankAccounts ?.map((bankAccount: any) => {
-      if (bankAccount.id === accountId) {
-        bankAccount.accounts[potIndex].pendingPaymentAmount = paymentAmount
-      }
-
-      return bankAccount
-    }));
-  }
-
-  const refreshAccountDetails = async (accountId: string) => {
-    const updatedAccount = await props.revolutGetBankAccount(accountId);
-    setBankAccounts(bankAccounts ?.map((bankAccount: any) => {
-      if (bankAccount.id === accountId) {
-        return updatedAccount
-      }
-      return bankAccount
-    }));
-  }
-
-  const makePaymentFromAccountPot = async (accountId: string, potIndex: number) => {
-
-    const requestId = Math.random() * 10000000;
-    const accountToPayFrom = bankAccounts ?.find((account: any) => account.id === accountId);
-    let currencyToPayIn = accountToPayFrom.currency;
-
-    const { selectedCounterparty, pendingPaymentAmount } = accountToPayFrom.accounts[potIndex];
-
-    const counterPartySplit = selectedCounterparty.split(".");
-    const counterPartyObject: any = {
-      counterparty_id: counterPartySplit[0]
-    }
-
-    if (counterPartySplit[1]) {
-      counterPartyObject.account_id = counterPartySplit[1];
-    }
-
-    if (counterPartySplit[2]) {
-      currencyToPayIn = counterPartySplit[2];
-    }
-
-    const result = await props.revolutPostPayment(accountId, pendingPaymentAmount, currencyToPayIn, counterPartyObject, requestId);
-
-    if (result.state) {
-      getRevolutBankAccounts();
-    }
-  }
-
-  const renderRevolut = () => {
-
-    return <>
-
-      {bankAccounts && <div style={{ marginBottom: 50 }}><ReactJson collapsed src={bankAccounts} /></div>}
-
-      {bankAccounts && <Blocks>
-        {bankAccounts ?.filter((account: any) => blinkMarkets.find(market => market.currency === account.currency))
-        .map((account: any) => {
-            const country = blinkMarkets.find(market => market.currency === account.currency);
-            return account ?.accounts ?.map((pot: any, potIndex: number) => {
-
-              if (potIndex > 0) {
-                return false;
-              }
-
-              let currencyToPayIn = account.currency;
-              if (pot.selectedCounterparty) {
-                const selectedCounterpartySplit = pot.selectedCounterparty.split(".");
-                if (selectedCounterpartySplit[2]) {
-                  currencyToPayIn = selectedCounterpartySplit[2];
-                }
-              }
-
-              return <Box key={`${account.id}-${pot.iban || pot.sort_code}`} title={''} icon={""} paddedLarge shadowed>
-                {/* <Account> */}
-
-                <TimeStamp>Updated: {new Date(account.updatedAt).toLocaleString('en-GB')}
-                  <Refresh onClick={() => refreshAccountDetails(account.id)} src={refreshIcon} />
-                </TimeStamp>
-
-                <Icon><img src={country ?.flag} /></Icon>
-
-                <AccountName>{account.name}</AccountName>
-
-                <AccountDetails>
-                  <AccountBalance>{(currencySymbols[account.currency] || account.currency)}{account.balance ?.toFixed(2)}</AccountBalance>
-                  <Item>BLINK ACCOUNT </Item>
-                  {pot.iban && <Item>IBAN: {pot.iban}</Item>}
-                  {pot.bic && <>BIC: {pot.bic}</>}
-                  {pot.account_no && <Item>Account no: {pot.account_no}</Item>}
-                  {pot.sort_code && <>Sort code: {pot.sort_code}</>}
-                </AccountDetails>
-
-                {/*  
-                <Item>BLINK ACCOUNT </Item>
-
-                {
-                  account.accounts?.map((pot: any) => <>
-                    {pot.iban && <>IBAN: {pot.iban}</>}
-                    {pot.account_no && <Item>Account no: {pot.account_no}</Item>}
-                    {pot.sort_code && <>Sort code: {pot.sort_code}</>}
-                  </>
-                )}
-                */}
-                {counterparties && <div>
-                  Make a payment to:  <select value={pot.selectedCounterparty} onChange={e => updateSelectedAccountCounterparty(account.id, potIndex, e.target.value)}>
-                    <option value="" disabled>Please select</option>
-                    {counterparties ?.map((counterparty: any) => {
-
-                      if (counterparty.accounts && counterparty.accounts.length > 0) {
-                        return counterparty.accounts.map((counterpartyAccount: any) => {
-                          return <option key={counterpartyAccount.id} value={`${counterparty.id}.${counterpartyAccount.id}.${counterpartyAccount.currency}`}>{counterpartyAccount.name} ({counterpartyAccount.currency})</option>
-                        });
-                      }
-
-                      return <option key={counterparty.id} value={counterparty.id}>{counterparty.name}</option>
-                    }
-                    )}
-                  </select>
-
-                  {pot.selectedCounterparty !== "" && <><span style={{ marginLeft: 10 }}>{(currencySymbols[currencyToPayIn] || currencyToPayIn)} </span>
-                    <input style={{ width: 50, padding: 10, lineHeight: "10px" }} type="text" value={pot.pendingPaymentAmount} onChange={(e: any) => updatePotPendingPaymentAmount(account.id, potIndex, e.target.value)} /></>}
-
-                  {pot.pendingPaymentAmount !== "" && !isNaN(parseFloat(pot.pendingPaymentAmount)) && parseFloat(pot.pendingPaymentAmount) > 0 && <button onClick={() => makePaymentFromAccountPot(account.id, potIndex)} style={{ fontSize: 12, marginLeft: 30 }}>Pay!</button>}
-
-                </div>}
-
-                {!pot.connections ?.xero && <LinkAccount onClick={async () => {
-                  const result = await props.xeroConnectBankAccount(account.id,
-                    account.currency,
-                    pot,
-                    account.name);
-
-                  if (result.success) {
-                    refreshAccountDetails(account.id)
-                  }
-                }}>Link</LinkAccount>}
-
-              </Box>
-            })
-          })}
-
-      </Blocks>}
-
-
-    </>
   }
 
   const renderDeleteAccountInput = () => <>
@@ -345,29 +159,19 @@ const Integrations = (props: any) => {
     }
   }
 
-  if (provider === undefined || provider === "revolut") {
-    if (bankAccounts === undefined && counterparties === undefined && transactions === undefined) {
-      // setBankAccounts([])
-      getRevolutBankAccounts();
-    }
-    if (bankAccounts !== undefined && counterparties === undefined && transactions === undefined) {
-      // setCounterparties([])
-      getRevolutCounterparties();
-    }
-    if (bankAccounts !== undefined && counterparties !== undefined && transactions === undefined) {
-      // setBankAccountTransactions([])
-      getRevolutBankAccountTransactions();
-    }
-  }
-
-
   return <Styled.MainSt>
     <User />
     <Styled.Content>
       {(provider === undefined || provider === "xero")
         && renderXero()}
-      {(provider === undefined || provider === "revolut")
-        && renderRevolut()}
+      {(provider === undefined || provider === "revolut") &&
+        <Revolut
+          revolutGetBankAccounts={props.revolutGetBankAccounts}
+          revolutGetBankAccount={props.revolutGetBankAccount}
+          // revolutPostPayment={props.revolutPostPayment}
+          revolutGetCounterparties={props.revolutGetCounterparties}
+          revolutGetBankAccountTransactions={props.revolutGetBankAccountTransactions}
+          revolutPostPayment={props.revolutPostPayment} />}
     </Styled.Content>
 
   </Styled.MainSt>
