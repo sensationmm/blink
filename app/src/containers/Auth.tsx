4f436e577f61e13453c6a88e5b3312000c1ba5d8
Nick Procopiou <nick.procopiou@11fs.com>
2020-04-15 14:40:55 +0100

    show the button on staging too

--- app/src/containers/Auth.tsx
+++ app/src/containers/Auth.tsx
@@ -31,7 +31,6 @@ const Auth = (props: any) => {
     const [hasRequestedSignInWithToken, setHasRequestedSignInWithToken] = useState(false);
     const [showSignUp, setshowSignUp] = useState(false);
 
-
     const token = window.localStorage.getItem("firebase-token");
     if (token && !hasRequestedSignInWithToken) {
         setHasRequestedSignInWithToken(true);
@@ -78,7 +77,7 @@ const Auth = (props: any) => {
                     }
 
                 </Styled.AuthWrapper>
-                {!showSignUp && window.location.href.startsWith("http://localhost:3000") && <button onClick={() => setshowSignUp(true)}>Show sign up</button>}
+                {!showSignUp && (window.location.href.startsWith("http://localhost:3000" || window.location.href.startsWith("https://blink-staging-20006.firebaseapp.com/")) && <button onClick={() => setshowSignUp(true)}>Show sign up</button>}
             </>
             <Actions>
                 {
