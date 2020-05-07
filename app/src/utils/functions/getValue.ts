e2ec91b227d94d6c8fd846886317d90937ab2238
Kevin Reynolds <kevin.reynolds@11fs.com>
2020-05-07 11:19:49 +0100

    Business details checkboxes

--- app/src/utils/functions/getValue.ts
+++ app/src/utils/functions/getValue.ts
@@ -1,7 +1,17 @@
 const getValue = (base: { [key: string]: any }, key: string = 'value') => {
+    let value;
     if (base) {
-        return base[key];
+        value = base[key];
     }
+
+    // @TODO- Hacky boolean value check
+    if (value === 'true' || value === 'True') {
+        value = true;
+    } else if (value === 'false' || value === 'False') {
+        value = false;
+    }
+
+    return value;
 };
 
 export default getValue;
